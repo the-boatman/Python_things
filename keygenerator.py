d5bd7c07e888c3238e5bf64a714f40674b8096ff
the-boatman <108884217+the-boatman@users.noreply.github.com>
2022-07-07 18:53:15 -0400

    Delete keygenerator.py

--- keygenerator.py
+++
@@ -1,9 +0,0 @@
-
-from posixpath import join
-# a cool tool i created for generating encryption keys
-import random as r
-plaintext = 'abcdefghijklmnopqrstuvwxyz 0123456789'
-listtext = list(plaintext)
-key = r.sample(listtext, k=37)
-output = ''.join(key)
-print(output)
