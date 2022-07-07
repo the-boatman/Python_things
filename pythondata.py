7df05abc5f914bf355861f091b1e953c59253322
the-boatman <108884217+the-boatman@users.noreply.github.com>
2022-07-07 12:19:33 -0400

    Update pythondata.py

--- pythondata.py
+++ pythondata.py
@@ -3,7 +3,7 @@ import random as r
 
 # this creates the 'real' data so we have something to analyze
 def writeARealCallExchange():
-    t = r.randint(1400,2100)
+    t = r.randint(1000,2359)
     dur = r.randint(15,60)
     ftm = r.sample([fran,mule], k=2)
     caller, callee = ftm
@@ -19,14 +19,14 @@ def writeARealCallExchange():
 def writefakedata():
     dur = r.randint(15,60)
     t = r.randint(1400,2100)
-    b = r.randint(1000,9999)
-    c = r.randint(1000,9999)
+    b = r.randint(6000,6999)
+    c = r.randint(6000,6999)
     data = [f'{a}-{b}',dur,f'{a}-{c}',t]
     writer.writerow(data)
 
 # define the 'real' phone numbers we care about "a" enforces the 555 area code
 fran = '555-6969'
-mule = '555-3452'
+mule = '555-6452'
 actcell = '555-6632'
 # the collum titles
 header = ['phone_number', 'call_duration', 'recipient', 'time']
